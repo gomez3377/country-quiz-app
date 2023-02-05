d1d30c6a3305b56d18a27ccac416635d1f971f70
gomez3377 <jongomez3377@gmail.com>
2023-02-05 00:21:14 -0700

    friday 2/3/23 changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
+import QuizContainer from './components/quiz-container/quiz-container'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quiz, setQuiz] = useState([])
+
+
+  useEffect(() => {
+      fetch('https://restcountries.com/v2/all')
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+
+
+  }
+  
+  
+  , [])
+
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h2>Country Quiz</h2>
+      <img alt='quiz-logo'  />
+      <QuizContainer />
     </div>
   )
 }
---
+++ src/components/quiz-container/quiz-container.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Answer from '../answer/answer.component'
+
+const QuizContainer = ( {question} ) => {
+  return (
+    <div>
+        <h3 className='question'>{question}</h3>
+        <div>
+            <Answer/>
+        </div>
+    </div>
+  )
+}
+
+export default QuizContainer
